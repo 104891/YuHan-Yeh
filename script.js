c7561a131065042f18e6fbe4c68fdba5100d889f
104891 <85594975+104891@users.noreply.github.com>
2026-07-17 17:10:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,155 @@
+function calculateResult(){
+
+    var mondayHours = Number(document.querySelector("#mondayHours").value);
+
+    var tuesdayHours = Number(document.querySelector("#tuesdayHours").value);
+
+    var wednesdayHours = Number(document.querySelector("#wednesdayHours").value);
+
+    var thursdayHours = Number(document.querySelector("#thursdayHours").value);
+
+    var fridayHours = Number(document.querySelector("#fridayHours").value);
+
+    var saturdayHours = Number(document.querySelector("#saturdayHours").value);
+
+    var sundayHours = Number(document.querySelector("#sundayHours").value);
+
+
+
+    var mondayLunch = Number(document.querySelector("#mondayLunch").value);
+
+    var tuesdayLunch = Number(document.querySelector("#tuesdayLunch").value);
+
+    var wednesdayLunch = Number(document.querySelector("#wednesdayLunch").value);
+
+    var thursdayLunch = Number(document.querySelector("#thursdayLunch").value);
+
+    var fridayLunch = Number(document.querySelector("#fridayLunch").value);
+
+    var saturdayLunch = Number(document.querySelector("#saturdayLunch").value);
+
+    var sundayLunch = Number(document.querySelector("#sundayLunch").value);
+
+
+
+    var totalStudyHours = mondayHours
+                        + tuesdayHours
+                        + wednesdayHours
+                        + thursdayHours
+                        + fridayHours
+                        + saturdayHours
+                        + sundayHours;
+
+
+
+    var averageStudyHours = totalStudyHours / 7;
+
+
+
+    var totalLunchExpense = mondayLunch
+                          + tuesdayLunch
+                          + wednesdayLunch
+                          + thursdayLunch
+                          + fridayLunch
+                          + saturdayLunch
+                          + sundayLunch;
+
+
+
+    var averageLunchExpense = totalLunchExpense / 7;
+
+
+
+    document.querySelector("#totalStudyHours").innerHTML = totalStudyHours;
+
+    document.querySelector("#averageStudyHours").innerHTML = averageStudyHours;
+
+    document.querySelector("#totalLunchExpense").innerHTML = totalLunchExpense;
+
+    document.querySelector("#averageLunchExpense").innerHTML = averageLunchExpense;
+
+
+
+    if(totalStudyHours >= 14){
+
+        document.querySelector("#studyMessage").innerHTML =
+        "You studied enough this week. Good job!";
+
+    }
+
+    else{
+
+        document.querySelector("#studyMessage").innerHTML =
+        "You may need to increase your study time next week.";
+
+    }
+
+
+
+    if(totalLunchExpense <= 7000){
+
+        document.querySelector("#lunchMessage").innerHTML =
+        "Your weekly lunch expense is within the suggested budget.";
+
+    }
+
+    else{
+
+        document.querySelector("#lunchMessage").innerHTML =
+        "Your lunch expense is high. Try to reduce your spending next week.";
+
+    }
+
+}
+
+
+
+function clearResult(){
+
+    document.querySelector("#mondayHours").value = "";
+
+    document.querySelector("#tuesdayHours").value = "";
+
+    document.querySelector("#wednesdayHours").value = "";
+
+    document.querySelector("#thursdayHours").value = "";
+
+    document.querySelector("#fridayHours").value = "";
+
+    document.querySelector("#saturdayHours").value = "";
+
+    document.querySelector("#sundayHours").value = "";
+
+
+
+    document.querySelector("#mondayLunch").value = "";
+
+    document.querySelector("#tuesdayLunch").value = "";
+
+    document.querySelector("#wednesdayLunch").value = "";
+
+    document.querySelector("#thursdayLunch").value = "";
+
+    document.querySelector("#fridayLunch").value = "";
+
+    document.querySelector("#saturdayLunch").value = "";
+
+    document.querySelector("#sundayLunch").value = "";
+
+
+
+    document.querySelector("#totalStudyHours").innerHTML = "0";
+
+    document.querySelector("#averageStudyHours").innerHTML = "0";
+
+    document.querySelector("#totalLunchExpense").innerHTML = "0";
+
+    document.querySelector("#averageLunchExpense").innerHTML = "0";
+
+
+
+    document.querySelector("#studyMessage").innerHTML = "";
+
+    document.querySelector("#lunchMessage").innerHTML = "";
+
+}
